5266ae4370c1136b8458cc2166e01a71faaa4938
Jordan Salvi <jsalvi1@ford.com>
2020-05-04 20:01:18 -0400

    wip

--- flask_server/server.py
+++ flask_server/server.py
@@ -5,4 +5,8 @@ client = MongoClient('localhost', 27017)
 db = client['scratchpad-db']
 app = Flask(__name__)
 
+@app.route('/')
+def health():
+    return "OK", 200
+
 app.run()
---
+++ simple_frontend/workout.js
@@ -0,0 +1,7 @@
+let contentArea;
+window.onload = function() {
+};
+
+function randomize() {
+    document.getElementById("content-area").innerHtml = Math.random() * 10;
+}
