65b18b8fa2f93a137f6bf980b2b16896370c9311
s4lvi <salvicomputers@gmail.com>
2020-05-06 01:20:49 -0400

    working workout randomizer

--- simple_frontend/workout.js
+++ simple_frontend/workout.js
@@ -1,7 +1,23 @@
-let contentArea;
-window.onload = function() {
-};
+var workouts = new Map();
 
-function randomize() {
-    document.getElementById("content-area").innerHtml = Math.random() * 10;
+function update() {
+    let type = document.getElementById("workout-input");
+    if (type.value != "") {
+        workouts.set(type.value,0);
+        type.value = "";
+        refresh();
+    }
+}
+
+function refresh() {
+    var contentArea = document.getElementById("content-area");
+    contentArea.innerHTML = "<ul>";
+    for (let w of workouts) {
+        var [key, value] = w;
+        value += 5 * (Math.floor(Math.random() * 2) + 1);
+        contentArea.innerHTML += "<li>" + key + ": " + value + "</li>";
+    }
+    contentArea.innerHTML += "</ul>";
+    console.log(workouts);
+    console.log(contentArea);
 }
