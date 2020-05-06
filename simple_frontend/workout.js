0d2504d4191006053108211dba68e125a029df5b
s4lvi <salvicomputers@gmail.com>
2020-05-06 02:13:22 -0400

    wip

--- simple_frontend/workout.js
+++ simple_frontend/workout.js
@@ -13,14 +13,14 @@ function remove(key) {
     workouts.delete(key);
     refresh();
 }
-
+// redo this using a set group capacity, ex 5 so that it loops 5 times through the map and adds a group to a random element
 function refresh() {
     var contentArea = document.getElementById("content-area");
     contentArea.innerHTML = "<ul>";
     for (let w of workouts) {
         var [key, value] = w;
-        value += 5 * (Math.floor(Math.random() * 2) + 1);
-        contentArea.innerHTML += "<li>" + key + ": " + value + " (<a href='#' onclick='remove(\"" + key + "\")'>x</a>)</li>";
+        value += (Math.floor(Math.random() * 2) + 1);
+        contentArea.innerHTML += "<li>" + key + ": " + value*5 + " (<a href='#' onclick='remove(\"" + key + "\")'>x</a>)</li>";
     }
     contentArea.innerHTML += "</ul>";
     console.log(workouts);
