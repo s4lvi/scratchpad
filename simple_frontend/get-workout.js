94f1de9fe00dc5bebbda238a774153c3765a1f8a
Jordan Salvi <jsalvi1@ford.com>
2020-05-12 01:00:00 -0400

    Added config form

--- simple_frontend/get-workout.js
+++ simple_frontend/get-workout.js
@@ -2,17 +2,32 @@ const client = stitch.Stitch.initializeDefaultAppClient('dailyworkout-kgzuu');
 
 const db = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('dailyWorkout');
 
+
+
+var form = document.getElementById("form");
+
+function handleForm(event) { 
+    event.preventDefault(); 
+} 
+
+form.addEventListener('submit', handleForm);
+
+let exercises = {};
 client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(() =>
   db.collection('exercises').find().asArray()
 ).then(docs => {
     if (docs.length > 0) {
         document.getElementById("workout-container").removeAttribute("hidden");
         let exerciseList = document.getElementById("workout-list");
-        let selectedExercises = docs;
-        for (let exercise of selectedExercises) {
+        exercises = docs;
+        for (let exercise of exercises) {
             exerciseList.innerHTML += "<li>" + exercise["name"] + "</li>";
         }
     }
 }).catch(err => {
   console.error(err)
 });
+
+function generate() {
+  console.log(exercises);
+}
